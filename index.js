64ce7da2376c111c1c31c0035f7df48b51eb754a
Min Gee Suh <m.suh1@student.unimelb.edu.au>
2020-03-23 16:33:13 +0900

    Cookie Session
    Initiallizing Cookie Session

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
-require('./service/passport')
 require('./models/User')
+require('./service/passport')
+
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 24 * 60 * 60 * 1000, //max 1 day
+        keys: [keys.cookieKey]
+    })
+)
+
+//ititializing passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 
--- service/passport.js
+++ service/passport.js
@@ -1,7 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+})
 
 passport.use(
     new GoogleStrategy(
@@ -11,8 +24,17 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         function(accessToken, refreshToken, profile, done) {
-            console.log('access token', accessToken )
-            console.log('refresh token', refreshToken)
-            console.log('profile', profile)
+
+           User.findOne({googleId: profile.id})
+               .then((existingUser) => {
+                   if (existingUser) { // if user already exists
+                       done(null, existingUser);
+
+                   } else { // no user exist with current profile Id
+                       new User({googleId:profile.id}).save()
+                           .then(newUser => done(null, newUser))
+                   }
+               })
+
         }
     ));
